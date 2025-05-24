2c7b698a4a7699f1a228d2df1373db3819100fff
Yunus Alav <alwwyunus@gmail.com>
2025-05-24 16:22:26 +0300

    web terms and conditions added

---
+++ PHP/terms.php
@@ -0,0 +1,173 @@
+  <div class="modal fade" id="generalTerms" data-backdrop="static" data-keyboard="false" tabindex="-1"
+      aria-labelledby="staticBackdrop" aria-hidden="true">
+      <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">General Terms And Conditions</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+          </div>
+          <div class="modal-body modal-fullscreen-md-down"  >
+              <p style="text-align: center; width: 100%;">
+
+                These Terms of Use contain the rules that you must follow when
+                using the website at this website. Please read these terms carefully before continuing to use the
+                website.
+
+              </p> <br>
+              
+              <strong>
+
+                Scope and Changes
+
+              </strong>
+              <p style="text-align: center; width: 100%;">
+
+                The website at example.com is operated by the Company. These
+                Terms of Use determine the rules to be applied during the use of the website and the rights and
+                obligations of the parties. If you continue to use the website, you will be deemed to have accepted
+                these terms.
+
+              </p> <br>
+              <strong>
+
+                Service Content and Use
+
+              </strong>
+              <p style="text-align: center; width: 100%;">
+
+                The services provided by this website are designed to be used
+                in accordance with legal regulations. Users are obliged to comply with all laws, statutes and
+                regulations, international agreements and customs while using the website. The Company reserves the
+                right to change, suspend or terminate its services at any time, for any reason, without prior
+                notice.
+
+              </p> <br>
+              <strong>
+
+                User Obligations
+
+              </strong>
+              <p style="text-align: center; width: 100%;">Users of this website are obliged to comply with the following rules: </p> <br>
+              <ul class="nav flex-column">
+                <li style="text-align: center; width: 100%;">Not to share content that is against the law or constitutes a crime.</li>
+                <li style="text-align: center; width: 100%;">Not to take any action that would violate the intellectual property rights of third parties.</li>
+                <li style="text-align: center; width: 100%;"> Not to spread malware or exploit vulnerabilities.</li>
+                <li style="text-align: center; width: 100%;"> Not to use the services provided in a manner contrary to their purpose or to misuse them.</li>
+                <li style="text-align: center; width: 100%;"> Not to engage in behavior that would prevent other users from benefiting from the services provided.</li>
+              
+                </ul>
+              </p>
+                
+              <strong>
+
+                Privacy Policy
+
+              </strong>
+              <p style="text-align: center; width: 100%;">
+
+                By continuing to use this website,ou are agreeing
+                <a href="#"> to our Privacy Policy . Please review our Privacy Policy for
+                  detailed information on how your personal data is collected, used and protected.
+                </a>
+
+              </p> <br>
+              <strong> Intellectual Property Rights</strong>
+              <p style="text-align: center; width: 100%;">
+
+                All content, design, logo, software and other materials on the
+                website are the Company's intellectual property and copyrights and are protected by the law on
+                intellectual and artistic works and other legal regulations. Unauthorized use, copying,
+                reproduction, modification or distribution of these contents is prohibited.
+
+              </p> <br>
+              <strong>
+
+                General Provisions
+
+              </strong>
+              <p style="text-align: center; width: 100%;">
+                <ul class="nav flex-column">
+                  <li style="text-align: center; width: 100%;">
+                    By continuing to use the website, all users accept and
+                    undertake that they will use this platform in a legal and ethical manner and only for personal
+                    purposes, and that they will not engage in actions that violate the rights of third parties.
+
+
+                  </li>
+                  <li style="text-align: center; width: 100%;">
+                    Users are responsible for their own legal and criminal
+                    responsibilities for their actions and transactions on the website.
+
+                  </li>
+                  <li style="text-align: center; width: 100%;">
+                    We do our best to ensure the accuracy and up-to-dateness of
+                    the information on the website. However, despite all our efforts, this information may lag behind
+                    actual changes and there may be some differences.
+
+                  </li>
+                  <li style="text-align: center; width: 100%;">
+                    The website may contain hyperlinks to other websites,
+                    applications and platforms operated by third parties, the contents of which are unknown to us. The
+                    Company only provides access to these sites and assumes no responsibility for their content.
+
+
+                  </li>
+              <li style="text-align: center; width: 100%;">
+
+                    Although we do our best to keep the website free of viruses,
+                    we do not guarantee that it is completely free of viruses. Therefore, it is the responsibility of
+                    users to take the necessary precautions against viruses when downloading data.
+
+                  </li>
+                </ul>
+                </p>
+              <strong>
+
+                Limits of Liability
+
+              </strong>
+              <p style="text-align: center; width: 100%;">
+
+                The Company shall not be liable for any direct, indirect,
+                special or punitive damages that may arise from the use of the Website. Our Website is provided "as
+                is" and no guarantee is given that it will operate uninterruptedly or without errors.
+
+              </p> <br>
+              <strong>
+
+                Dispute Resolution
+
+              </strong>
+              <p style="text-align: center; width: 100%;">
+
+                In case of any disputes that may arise from the use of this
+                website, the courts and enforcement offices of Istanbul will have jurisdiction. These conditions
+                will be interpreted and applied in accordance with the laws of Türkiye.
+
+              </p> <br>
+              <strong>
+
+                Final Provisions
+
+              </strong>
+              <p style="text-align: center; width: 100%;">
+
+                These Terms of Use entered into force on
+                <strong>
+
+                  March 25, 2025
+
+                </strong>
+
+                , and if you continue to use the website, you will be deemed
+                to have accepted these Terms of Use. The invalidity of any provision in the Terms of Use does not
+                affect the validity of the other provisions.
+
+              </p> <br>
+              <button type="button" class="btn btn-danger btn-sm" data-bs-dismiss="modal">Close</button>
+
+          </div>
+        </div>
+      </div>
+   
+    </div>
