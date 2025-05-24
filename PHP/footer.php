14c6cc3c20840293398e34a4f3c231a4d63242ea
Yunus Alav <alwwyunus@gmail.com>
2025-05-24 16:07:41 +0300

    footer part added

---
+++ PHP/footer.php
@@ -0,0 +1,32 @@
+ 
+ <style>
+  #footer{
+    width: auto;
+    height: auto;
+  }
+ </style>
+<!-- FontAwesome 6.2.0 CSS -->
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+  integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>
+ <!-- FOOTER -->
+    <footer class=" mt-5" id="footer">
+      <p class=" text-white"><a href="#">Back to top</a></p>
+      <div class="row">
+         <p>&copy; 2017-2020 Company, Inc.   </p>
+        <p class="col-4">
+          &middot;<a class=" text-white active "data-bs-toggle="modal" data-bs-target="#generalTerms">General Terms and Conditions </a>
+        </p>
+      <p class="col-4 text-center text-white">Fallow Us:<br>
+      <i class="m-2 fa-brands fa-instagram  fa-2xl" style="color: #ffffff;"></i>
+      <i class=" m-2 fa-brands fa-facebook fa-2xl" style="color: #005eff;"></i>
+      <i class="m-2 fa-brands fa-linkedin fa-2xl" style="color: #74C0FC;"></i>
+      </div> </p>
+      
+     
+    </footer>
+        <?php include "terms.php"; ?>
